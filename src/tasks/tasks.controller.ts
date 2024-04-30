edc8ae0ef6e1727838906f48300d7dae5ade399f
Abderrahmane <abderrahmanekihal5@gmail.com>
2024-04-30 03:01:14 +0100

    get tasks by id and update and delete tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,16 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateTaskDto } from 'src/dto/create-task.dto';
+import { GetTaskFilterDto } from 'src/dto/get-task-filter.dto';
+import { UpdateTaskStatusDto } from 'src/dto/update-task-status.dto';
 import { Task } from '../models/task.model';
 import { TasksService } from './tasks.service';
 
@@ -9,13 +20,41 @@ export class TasksController {
     this.tasksService = tasksService;
   }
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: GetTaskFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
   }
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
-    console.log('Received createTaskDto:', createTaskDto);  
+    console.log('Received createTaskDto:', createTaskDto);
 
     return this.tasksService.createTask(createTaskDto);
+  }
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+  @Patch('/:id')
+  updateTask(
+    @Param('id') id: string,
+    @Body('field') field: keyof Task,
+    @Body('value') value: any,
+  ): Task {
+    return this.tasksService.updateTaskField(id, field, value);
+  }
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  ): Task {
+    const { status } = updateTaskStatusDto;
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from 'src/dto/create-task.dto';
 import { v4 as uuid } from 'uuid';
 
+import { GetTaskFilterDto } from 'src/dto/get-task-filter.dto';
 import { Task, TaskStatus } from '../models/task.model';
 
 @Injectable()
@@ -14,29 +15,58 @@ export class TasksService {
 
     return this.tasks;
   }
+  getTasksWithFilters(filterDto: GetTaskFilterDto): Task[] {
+    const { status, search } = filterDto;
+    let tasks = this.getAllTasks();
+    if (status) {
+      tasks = tasks.filter((task) => task.status.includes(status));
+    }
+    if (search) {
+      tasks = tasks.filter(
+        (task) =>
+          task.title.includes(search) || task.description.includes(search),
+      );
+    }
+    return tasks;
+  }
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description, images } = createTaskDto;
-   if (!createTaskDto.images) {
-    this.logger.error('Images array is undefined');
-  } else {
-    this.logger.debug('Images array:', JSON.stringify(createTaskDto.images));
-  }
-    const mappedImages = images.map(image => ({
-      id: uuid(), 
-      url: image.url  
+
+    const mappedImages = images.map((image) => ({
+      id: uuid(),
+      url: image.url,
     }));
     const task: Task = {
-      id: uuid(), 
+      id: uuid(),
       title,
       description,
       status: TaskStatus.OPEN,
-      images:mappedImages ,
-      };
-      
-     
-      
-    
+      images: mappedImages,
+    };
+
     this.tasks.push(task);
     return task;
+  }
+  getTaskById(id: string): Task {
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    } else {
+      return found;
+    }
+  }
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+  updateTaskField(id: string, field: keyof Task, value: any): Task {
+    const task = this.getTaskById(id);
+    task[field] = value;
+    return task;
+  }
+  deleteTask(id: string): void {
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== found.id);
   }
 }
